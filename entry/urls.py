e47a5684d951bc59b81d6f305d923f3957248519
Caleigh <minshallc@gmail.com>
2016-12-23 20:35:24 -0500

    Added view, template for entry details, updated css

--- entry/urls.py
+++ entry/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.entry_list, name='entry_list')
+	url(r'^$', views.entry_list, name='entry_list'),
+	url(r'^entry/(?P<pk>\d+)/$', views.entry_detail, name='entry_detail'),
 ]
--- entry/views.py
+++ entry/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Entry
 
 def entry_list(request):
 	entries = Entry.objects.filter(Date__lte=date.today()).order_by('Date')
 	return render(request, 'entry/entry_list.html', {'entries': entries})
+
+def entry_detail(request, pk):
+	entry = get_object_or_404(Entry, pk=pk)
+	return render(request, 'entry/entry_detail.html', {'entry': entry})
