f0852d7a58a408d68b593e8dbc701ace093333e1
minshallc@gmail.com <minshallc@gmail.com>
2017-02-11 14:57:46 -0800

    additional files and changes for Heroku

--- rdj/settings.py
+++ rdj/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'rdj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rdj',
+        'USER': 'caleighm',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
